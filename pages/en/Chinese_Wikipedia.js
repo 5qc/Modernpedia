8384ffe8cf76326f2bd6b1c57fef38f8fdba1229
ro <ro@umlike.wtf>
2021-06-07 19:52:55 -0700

    Create Chinese_Wikipedia.js

---
+++ pages/en/Chinese_Wikipedia.js
@@ -0,0 +1,16 @@
+// Change favicon
+setTimeout(function() {
+  var link = document.querySelector("link[rel~=\"icon\"]");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.href = "https://cdn.glitch.com/eb49ccc8-ffaa-44bb-8f04-39053cb25379%2Fcensored%20wikipedia.ico?v=1623106779852";
+}, 0);
+
+// ==UserScript==
+// @match        https://en.wikipedia.org/wiki/Chinese_Wikipedia
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
