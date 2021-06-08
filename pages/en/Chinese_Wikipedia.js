5b51e0c012417d41df3b9e0abba3011a8d14f497
ro <ro@umlike.wtf>
2021-06-07 19:51:12 -0700

    Update Chinese_Wikipedia.js

--- pages/en/Chinese_Wikipedia.js
+++ pages/en/Chinese_Wikipedia.js
@@ -1,9 +1,3 @@
-// ==UserScript==
-// @match        https://en.wikipedia.org/wiki/Chinese_Wikipedia
-// @run-at       document-start
-// @grant        GM_addStyle
-// ==/UserScript==
-
 // Change favicon
 setTimeout(function() {
   var link = document.querySelector("link[rel~=\"icon\"]");
@@ -14,3 +8,9 @@ setTimeout(function() {
   }
   link.href = "https://cdn.glitch.com/eb49ccc8-ffaa-44bb-8f04-39053cb25379%2Fcensored%20wikipedia.ico?v=1623106779852";
 }, 0);
+
+// ==UserScript==
+// @match        https://en.wikipedia.org/wiki/Chinese_Wikipedia
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
